06a8bc342b33b6126b215fb2ffca129cdf7bbb1c
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2022-02-24 01:29:23 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -133,7 +133,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'rabhi1611@gmail.com'
-EMAIL_HOST_PASSWORD = 'abhishek0810@bdcoe'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
